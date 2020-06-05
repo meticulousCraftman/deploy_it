bc52b80fc9559718d1a43b4b6504908d0ebf6400
Apoorva Singh <apoorvasingh157@gmail.com>
2020-06-06 01:17:32 +0530

    created a deploy_it folder with config and readme file

--- deploy_it.py
+++ deploy_it.py
@@ -1,6 +1,9 @@
 from pyfiglet import Figlet
 from PyInquirer import prompt
 from jinja2 import Template
+import os
+
+os.mkdir("./deploy_it")
 
 banner = Figlet(font="slant").renderText("Deploy it")
 
@@ -8,13 +11,32 @@ banner = Figlet(font="slant").renderText("Deploy it")
 print("Answer the following questions to make your project ready for deployment")
 
 gunicorn_questions = [
-    {"type": "input", "name": "django_project_name", "message": "Django project name: "},
-    {"type": "input", "name": "username", "message": "What's the username that is being used on the server?"},
-    {"type": "input", "name": "working_directory", "message": "Enter absolute path of the project's working directory:"},
-    {"type": "input", "name": "venv_path", "message": "Enter the path to your venv folder:"},
-    {"type": "input", "name": "wsgi_path", "message": "Enter dot separated WSGI file location:"},
-    {"type": "input", "name": "wsgi_app_name", "message": "Name of WSGI app:"}
-
+    {
+        "type": "input",
+        "name": "django_project_name",
+        "message": "Django project name: ",
+    },
+    {
+        "type": "input",
+        "name": "username",
+        "message": "What's the username that is being used on the server?",
+    },
+    {
+        "type": "input",
+        "name": "working_directory",
+        "message": "Enter absolute path of the project's working directory:",
+    },
+    {
+        "type": "input",
+        "name": "venv_path",
+        "message": "Enter the path to your venv folder:",
+    },
+    {
+        "type": "input",
+        "name": "wsgi_path",
+        "message": "Enter dot separated WSGI file location:",
+    },
+    {"type": "input", "name": "wsgi_app_name", "message": "Name of WSGI app:"},
 ]
 
 gunicorn_answers = prompt(gunicorn_questions)
@@ -24,7 +46,7 @@ print("Generating file...")
 with open("gunicorn-template", "r") as f:
     template = Template(f.read())
     print("Gunicorn service file created!")
-    gunicorn_service_file = open("gunicorn.service", "w")
+    gunicorn_service_file = open("deploy_it/gunicorn.service", "w")
     gunicorn_service_file.write(template.render(gunicorn_answers))
     gunicorn_service_file.close()
 
@@ -33,8 +55,16 @@ print("Creating configuration file for Nginx")
 
 nginx_questions = [
     {"type": "input", "name": "server_ip_address", "message": "Server IP address:"},
-    {"type": "input", "name": "static_endpoint", "message": "What is the endpoint used to serve static content:"},
-    {"type": "input", "name": "staticfile_folder", "message": "Absolute path of folder with static assets:"},
+    {
+        "type": "input",
+        "name": "static_endpoint",
+        "message": "What is the endpoint used to serve static content:",
+    },
+    {
+        "type": "input",
+        "name": "staticfile_folder",
+        "message": "Absolute path of folder with static assets:",
+    },
 ]
 
 nginx_answers = prompt(nginx_questions)
@@ -45,12 +75,22 @@ with open("nginx-template", "r") as f:
     nginx_answers.update(gunicorn_answers)
     template = Template(f.read())
     print("Nginx service file created!")
-    nginx_file = open(nginx_answers['django_project_name'], "w")
+    nginx_file = open("deploy_it/" + nginx_answers["django_project_name"], "w")
     nginx_file.write(template.render(nginx_answers))
     nginx_file.close()
 
+# Creating a readme file inside deploy_it/ folder
+# under the current working directory
+with open("deploy_it/README.txt", "w") as f:
+    f.write(
+        "This folder contains the server configuration file for deploying the django project.\n"
+    )
+    f.write("And will also contain the log file for the project when it's deployed.")
+    f.close()
+
+
 # Run this script with sudo
-# Create gunicorn systemd service file
-# Create nginx server block file
-# Generates the files and places them in the local folder
+# Done - Create gunicorn systemd service file
+# Done - Create nginx server block file
+# Done - Generates the files and places them in the local folder
 # Copies the files to the required folder
